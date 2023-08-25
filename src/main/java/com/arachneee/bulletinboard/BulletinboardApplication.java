ba3647803368092702273aba737136e766f3c9b2
Arachne <jhg2819@naver.com>
2023-08-25 20:39:56 +0900

    start project with spring boot

---
+++ src/main/java/com/arachneee/bulletinboard/BulletinboardApplication.java
@@ -0,0 +1,13 @@
+package com.arachneee.bulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arachneee/bulletinboard/BulletinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arachneee.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
