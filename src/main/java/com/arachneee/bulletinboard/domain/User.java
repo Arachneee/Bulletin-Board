fa3c68f6c44beb7d1d62f2ef0a24d35e8215b268
Arachne <jhg2819@naver.com>
2023-08-27 17:45:51 +0900

    make domain class User

---
+++ src/main/java/com/arachneee/bulletinboard/domain/User.java
@@ -0,0 +1,11 @@
+package com.arachneee.bulletinboard.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private Long id;
+	private String loginId;
+	private String password;
+	private String name;
+}
