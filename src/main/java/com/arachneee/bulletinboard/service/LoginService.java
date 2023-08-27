2061d04f7633874f3d2edcdca28e9f82946130c7
Arachne <jhg2819@naver.com>
2023-08-27 19:58:41 +0900

    make LoginService

---
+++ src/main/java/com/arachneee/bulletinboard/service/LoginService.java
@@ -0,0 +1,22 @@
+package com.arachneee.bulletinboard.service;
+
+import org.springframework.stereotype.Service;
+
+import com.arachneee.bulletinboard.domain.member.Member;
+import com.arachneee.bulletinboard.domain.member.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+	private final MemberRepository memberRepository;
+
+	public Member login(String loginId, String password) {
+		return memberRepository.findByLoginId(loginId)
+			.filter(member -> member.getPassword().equals(password))
+			.orElse(null);
+	}
+
+}
