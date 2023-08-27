3499f2d292566992e5b717c9c69ad86c14fb7295
Arachne <jhg2819@naver.com>
2023-08-27 17:41:01 +0900

    make domain class Post

--- src/main/java/com/arachneee/bulletinboard/domain/Post.java
+++ src/main/java/com/arachneee/bulletinboard/domain/Post.java
@@ -1,5 +1,12 @@
 package com.arachneee.bulletinboard.domain;
 
+import java.time.LocalDate;
+
 public class Post {
 
+	private Long id;
+	private String title;
+	private String content;
+	private User user;
+	private LocalDate createTime;
 }
