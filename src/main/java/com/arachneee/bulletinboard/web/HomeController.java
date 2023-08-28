e17d380b685331e5d184741c23efe53dd3f666aa
Arachne <jhg2819@naver.com>
2023-08-28 23:06:56 +0900

    add Login annotation

--- src/main/java/com/arachneee/bulletinboard/web/HomeController.java
+++ src/main/java/com/arachneee/bulletinboard/web/HomeController.java
@@ -1,12 +1,11 @@
 package com.arachneee.bulletinboard.web;
 
+import com.arachneee.bulletinboard.web.argumentresolver.Login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.arachneee.bulletinboard.domain.member.Member;
-import com.arachneee.bulletinboard.web.session.SessionConst;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class HomeController {
 	@GetMapping("/")
-	public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+	public String home(@Login Member loginMember, Model model) {
 
 		if (loginMember == null) {
 			log.info("session login state false");
--- src/main/java/com/arachneee/bulletinboard/web/WebConfig.java
+++ src/main/java/com/arachneee/bulletinboard/web/WebConfig.java
@@ -1,10 +1,13 @@
 package com.arachneee.bulletinboard.web;
 
+import com.arachneee.bulletinboard.web.argumentresolver.LoginMemberArgumentResolver;
 import com.arachneee.bulletinboard.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
 
 
 @Configuration
@@ -16,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/", "/members/add", "/login", "/logout",
                         "/css/**", "/*.ico", "/error"
                 );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 }
